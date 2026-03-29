6dd97b5b427fec0893f6a0e340a59fc6366995c5
Z User <z@container>
2026-03-29 01:01:02 +0000

    v1.3.1 - Update version number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.aikeyboard"
         minSdk = 26
         targetSdk = 34
-        versionCode = 12
-        versionName = "1.2.0"
+        versionCode = 13
+        versionName = "1.3.1"
         
         buildConfigField("String", "GROQ_API_KEY", "\"$groqApiKey\"")
     }
